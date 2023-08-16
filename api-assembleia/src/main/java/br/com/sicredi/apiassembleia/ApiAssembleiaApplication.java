41effff9fd5959669cb90fd6e511acd400aa2813
Paulo Previlato <paulo.previlato@gmail.com>
2023-08-16 15:41:32 -0400

    Commit initial - Projeto Criado

---
+++ api-assembleia/src/main/java/br/com/sicredi/apiassembleia/ApiAssembleiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.apiassembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssembleiaApplication.class, args);
+	}
+
+}
---
+++ api-assembleia/src/test/java/br/com/sicredi/apiassembleia/ApiAssembleiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.apiassembleia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssembleiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
